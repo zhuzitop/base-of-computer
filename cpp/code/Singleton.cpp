0b5c6bb259b75cfc0e7f68c821a4423f2ad50aa4
zhuzi <zhuzitop@foxmail.com>
2019-03-04 11:41:36 +0800

    add code

---
+++ cpp/code/Singleton.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Singleton {
+public:
+	static Singleton* Instance();
+	
+protected:
+	Singleton();
+
+private:
+	static Singleton* _instance;
+	
+};
+
+Singleton* Singleton::_instance = 0;
+
+Singleton* Singleton::Instance() {
+	if (_instance == 0) {
+		_instance = new Singleton;
+	}
+	return _instance;
+}
+
+int main()
+{}
